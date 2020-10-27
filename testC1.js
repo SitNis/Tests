6b37e3bcfa32c2b5280e8bbb1e5f3c399ea50e36
SitNis <rus.lol2011@yandex.ru>
2020-10-28 02:46:23 +0300

    new C1

--- testC1.js
+++ testC1.js
@@ -1,19 +1,78 @@
-class safe{
-    constructor(){
-        this.a = new Map()
-    }
+// class safe{
+//     constructor(){
+//         this.a = new Map()
+//     }
   
-    save(key,value){
-        this.a.set(key,value)
+//     save(key,value){
+//         this.a.set(key,value)
+        
+//     }
+// }
+
+
+// let dict = new safe
+
+// dict.save(1,2)
+// dict.save(2,3)
+// dict.save(3,50)
+
+// console.log(dict.a)
+
+
+class dict {
+    constructor() {
+        this.hash = {
+            
         
     }
 }
+    checkOut(n){
+        let arr = Object.keys(this.hash)
+        if (arr.length < n){
+            return true
+        }
+        else {
+            console.log("error:out of range")
+            return false
+        }
+    }
+
+    adder(key,value){
+        if (this.checkOut(3) != true){
+            
+            return
+
+        }
+        if (key in this.hash){
+            return
+        }
+        else { 
+            this.hash[key] = value
+            return 
+        }
+        
+    }
+
+    deleter(key){
+        delete(this.hash[key])
+    }
+
+    set aChoOnDelaet(value){
+        this.hash[value] = this.hash[value] * 4
+    }
+    get aChoOnDelaet(){
+        return this.hash
+    }
+
+}
+
+let a = new dict
+a.adder(1,3)
+a.adder(2,4)
+a.adder(3,5)
+
+a.aChoOnDelaet = 3
+console.log(a.aChoOnDelaet)
 
 
-let dict = new safe
 
-dict.save(1,2)
-dict.save(2,3)
-dict.save(3,50)
-
-console.log(dict.a)
